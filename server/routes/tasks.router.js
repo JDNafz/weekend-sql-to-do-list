1431096ce9784b2ea5d7ae0a8193c0a4e4716e62
JD Nafziger <nafziger10@gmail.com>
2023-09-01 15:19:50 -0500

    missed a file

---
+++ server/routes/tasks.router.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('./pool');
+
+router.get('/', (req, res) => {
+    // console.log('server is req artist from db...')
+    let queryText = `
+    SELECT * FROM tasks 
+    ORDER BY "id" ASC;
+    `;
+    pool.query(queryText).then(
+        (result) => {
+            // console.log("DB returns result.rows: ", result.rows)
+            res.send(result.rows)
+        }
+    ).catch(
+        (err) => {
+            console.log(`Error making query ${queryText}`, err);
+            res.sendStatus(500);
+        }
+    )
+});
+
+
+router.post('/', (req, res) => {
+    let newTask = req.body
+    const queryText = `
+    INSERT INTO tasks ("task")
+    VALUES ($1);
+    `
+    pool.query(queryText, [newTask.task])
+        .then(
+            (result) => {
+                // console.log("db says Good submit to server!")
+                res.sendStatus(201);
+            })
+        .catch(
+            (err) => {
+                console.log(`Error making query ${queryText}`, err);
+                res.sendStatus(500);
+            }
+        )
+
+});
+
+router.delete('/:id', (req,res) => {0
+    let queryText = `
+    DELETE FROM tasks WHERE id = $1;
+    `;
+    pool.query(queryText, [req.params.id])
+        .then((result) => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log(`Error making query ${queryText}`,err );
+            res.sendStatus(500);
+        }
+    )
+});
+
+
+module.exports = router;
