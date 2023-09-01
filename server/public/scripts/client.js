9b5e32b156f5efaa7bc645f2ffdd8b25f5056f1e
JD Nafziger <nafziger10@gmail.com>
2023-09-01 14:47:17 -0500

    client.js get add delete render completed

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,158 +1,75 @@
 $(document).ready(onReady);
 
 function onReady() {
-    $('#submit-artist').on('click', function(event) {
-        event.preventDefault();
-        addArtist();
-    });
-    $('#submit-song').on('click', function(event) {
-        event.preventDefault();
-        addSong();
-    });
-    $('#artistTableBody').on('click', '.delete-btn', deleteArtist);
-    $('#songTableBody').on('click','.delete-btn', deleteSong);
+    $('#submit-btn').on('click', addTask);
+    $('#').on('click', '.delete-btn', deleteTask);
 
     // load data from the server, put it on the DOM
-    getArtists();
-    getSongs();    
+    getTasks();   
 }
 
-function deleteArtist (){
+function deleteTask (){
     // console.log("this.data:", $(this).parent().parent().data )
 
     let id = $(this).parent().parent().data('id')
     console.log('Delete id: ', id);
     $.ajax({
         method: 'DELETE',
-        url: `/artist/${id}`
+        url: `/tasks/${id}`
     }).then((req,res) => {
-        console.log('begon foul artist!')
-        getArtists();
+        console.log('begon foul task!')
+        getTasks();
     }).catch();
 
-} //end deleteArtist
+} //end deleteTask
 
-function deleteSong (){
-   // ADD ACCESS TO DATA
-   let id = (this).parent().parent().data('id')
-    console.log('Delete id: ', id);
-    $.ajax({
-        method: 'DELETE',
-        url: `/song/${id}`
-    }).then((req,res) => {
-        // console.log('begon foul song!')
-        getSongs();
-    }).catch();
 
-} //end deleteArtist
-
-function addArtist() {
+function addTask() {
     // Get info to send to the server
-    const artistToSend = {
-        name: $('#artist-name').val(), 
-        birthdate: $('#artist-born').val()
-    };
+    const taskToSend = { task: $('#todoIn').val() };
+    console.log('Adding task', taskToSend);
 
-    console.log('Adding artist', artistToSend);
-
-    // Send the new artist to the server as data
+    // Send the new task to the server
     $.ajax({
         method: 'POST',
-        url: '/artist',
-        data: artistToSend
+        url: '/tasks',
+        data: taskToSend
     }).then(function(response) {
         console.log(response);
-        getArtists();
+        getTasks();
     }).catch(function(error) {
-        console.log('error in artist post', error); 
-        alert('Error adding artist. Please try again later.')       
+        console.log('error in task post', error);    
     });
-}
+}//end addTask
 
-function addSong() {
-    // Get info to send to the server
-    const newSong = {
-        title: $('#song-name').val(), 
-        length: $('#song-length').val(),
-        released: $('#song-released').val()
-    };
 
-    console.log('Adding song', newSong);
-
-    // Send the new artist to the server as data
-    $.ajax({
-        method: 'POST',
-        url: '/song',
-        data: newSong
-    }).then(function(response) {
-        console.log(response);
-        getSongs();
-    }).catch(function(error) {
-        console.log('error in song post', error); 
-        alert('Error adding song. Please try again later.')       
-    });
-}
-
-function getArtists() { 
-    // get artist data from the server
+function getTasks() { 
+    // get task data from the server
     $.ajax({
         method: 'GET',
-        url: '/artist'
+        url: '/tasks'
     }).then(function(response) {
-        const listOfArtists = response;
-        renderArtists(response);
+        render(response);
     }).catch(function (error) {
-        console.log('error in artist get', error);
+        console.log('error in task get', error);
     });
-}
+}//end getTask
 
-function getSongs() {
-    // get song data from the server
-    $.ajax({
-        method: 'GET',
-        url: '/song'
-    }).then(function (response) {
-        renderSongs(response);
-    }).catch(function (error) {
-        console.log('error in song get', error);
-    });
-}
-
-function renderArtists( listOfArtists ) {
+function render( tasks ) {
     // Empty previous data
-    $('#artistTableBody').empty();
+    $('#todoTable').empty();
 
-    // Add all artists to table
-    for(let artist of listOfArtists) {
+    // Add all Tasks to table
+    for(let task of tasks) {
         
         let element = $(`
             <tr>
-                <td>${artist.name}</td>
-                <td>${artist.birthdate}</td>
+                <td>${task.name}</td>
                 <td>
                     <input class="delete-btn" class="btn" type="submit" value="remove">
                 </td>
             </tr>`);
-        element.data('id',artist.id)
-        $('#artistTableBody').append(element);
+        element.data('id',task.id)
+        $('#todoTable').append(element);
     }
-}
-
-function renderSongs(listOfSongs) {
-    // Empty previous data
-    $('#songTableBody').empty();
-    // Add all songs to table
-    for (let song of listOfSongs) {
-        let element = $(`
-            <tr>
-                <td>${song.title}</td>
-                <td>${song.length}</td>
-                <td>${song.released}</td>
-                <td>
-                    <input class="delete-btn" class="btn" type="submit" value="remove">
-                </td>
-            </tr>`);
-        element.data('id',song.id)
-        $('#songTableBody').append(element);
-    }
-}
+} //end render
