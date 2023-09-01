b3c1d468a6107ced431ec98697faf317c18c3a5b
JD Nafziger <nafziger10@gmail.com>
2023-09-01 09:01:21 -0500

    todo list for development added server.js

--- server/server.js
+++ server/server.js
@@ -8,67 +8,34 @@ const PORT = 5000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
-app.listen(PORT, () => {
-    console.log('listening on port', PORT)
-});
-
-// DONE - Replace static content with a database tables
-// const artistList = [ 
-//     {
-//         name: 'Ella Fitzgerald',
-//         birthdate: '04-25-1917'
-//     },
-//     {
-//         name: 'Dave Brubeck',
-//         birthdate: '12-06-1920'
-//     },       
-//     {
-//         name: 'Miles Davis',
-//         birthdate: '05-26-1926'
-//     },
-//     {
-//         name: 'Esperanza Spalding',
-//         birthdate: '10-18-1984'
-//     },
-// ]
-
-// const songList = [
-//     {
-//         title: 'Take Five',
-//         length: '5:24',
-//         released: '1959-09-29'
-//     },
-//     {
-//         title: 'So What',
-//         length: '9:22',
-//         released: '1959-08-17'
-//     },
-//     {
-//         title: 'Black Gold',
-//         length: '5:17',
-//         released: '2012-02-01'
-//     }
-// ];
-
-// app.get('/artist', (req, res) => {
-//     console.log(`In /songs GET`);
-//     res.send(artistList);
-// });
 let artistRouter = require('./routes/artist.router.js');
-app.use('/artist', artistRouter);
-
-let songRouter = require('./routes/song.router.js');
-app.use('/song', songRouter);
+app.use('/tasks', artistRouter);
 
 
-app.get('/song', (req, res) => {
-    console.log(`In /songs GET`);
-    res.send(songList);
+
+
+
+app.listen(PORT, () => {
+    console.log('Beep Boop I\'m on', PORT)
 });
 
-app.post('/song', (req, res) => {
-    songList.push(req.body);
-    res.sendStatus(201);
-});
+
+
+// set up front end html
+// set up db: weekend-to-do-app > tasks TABLE
+    // include database.sql text file
+    //
+
+
+// POST client task, server route, route Pool, SQL command 
+// DATAFORMAT { (id), complete, taskDescription} 
+// GET client tasks, server route, route pool, SQL command
+// RENDER client 
+    // client DELETE button
+// DELETE client task, server route, route pool, SQL command
+// Complete-btn/box client ajax, server route, route pool, SQL command
+
+//css styling on completed
+
 
 
